053d3e002a862ab5df366c17b370fcad6dd32d5f
Stas <94165517+StanTheMove@users.noreply.github.com>
2023-04-14 22:44:48 +0300

    Here is my updates

--- Program.cs
+++ Program.cs
@@ -8,8 +8,6 @@ namespace SeaBattle_Copy_paste____
 {
     internal class Program
     {
-        static int[,] playerBoard = new int[10, 10];
-        static int[,] computerBoard = new int[10, 10];
         static void PlaceShips(int[,] board)
         {
             Random random = new Random();
@@ -19,22 +17,171 @@ namespace SeaBattle_Copy_paste____
             {
                 int x = random.Next(0, 10);
                 int y = random.Next(0, 10);
-
-                // Перевірте, чи є корабель в цій позиції.
                 if (board[x, y] == 1)
                 {
-                    // Якщо так, повторіть спробу.
                     i--;
                 }
                 else
                 {
-                    // Якщо ні, розмістіть корабель.
                     board[x, y] = 1;
                 }
             }
         }
+        static void DrawBoard(int[,] board)
+        {
+            Console.Write("  ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + " ");
+                for (int j = 0; j < 10; j++)
+                {
+                    if (board[i, j] == 0)
+                    {
+                        Console.Write(". ");
+                    }
+                    else if (board[i, j] == 1)
+                    {
+                        Console.Write("S ");
+                    }
+                    else if (board[i, j] == 2)
+                    {
+                        Console.Write("X ");
+                    }
+                    else if (board[i, j] == 3)
+                    {
+                        Console.Write("O ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+        static void PlayerTurn(int[,] computerBoard)
+        {
+            Console.WriteLine("Enter coordinates to attack (for example A5):");
+            int x, y;
+            while (true)
+            {
+                string input = Console.ReadLine();
+                x = (int)input[0] - 65;
+                y = int.Parse(input.Substring(1)) - 1;
+
+                if (x < 0 || x > 9 || y < 0 || y > 9)
+                {
+                    Console.WriteLine("Invalid input. Please enter coordinates within the range of A1 to J10:");
+                }
+                else if (computerBoard[x, y] == 2 || computerBoard[x, y] == 3)
+                {
+                    Console.WriteLine("You have already attacked that position. Please choose a different position:");
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (computerBoard[x, y] == 1)
+            {
+                Console.WriteLine("Hit!");
+                computerBoard[x, y] = 2;
+            }
+            else
+            {
+                Console.WriteLine("Miss!");
+                computerBoard[x, y] = 3;
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+        static void ComputerTurn(int[,] playerBoard)
+        {
+            Console.WriteLine("Computer's turn:");
+
+            // Згенеруйте випадкові координати для атаки.
+            Random random = new Random();
+            int x, y;
+            do
+            {
+                x = random.Next(10);
+                y = random.Next(10);
+            } while (playerBoard[x, y] == 2 || playerBoard[x, y] == 3);
+
+            // Обробіть атаку на вказаних координатах.
+            if (playerBoard[x, y] == 1)
+            {
+                Console.WriteLine($"Computer hit {Convert.ToChar(x + 65)}{y + 1}!");
+                playerBoard[x, y] = 2;
+            }
+            else
+            {
+                Console.WriteLine($"Computer missed {Convert.ToChar(x + 65)}{y + 1}.");
+                playerBoard[x, y] = 3;
+            }
+
+            // Зачекайте на введення користувачем будь-якої клавіші, щоб продовжити гру.
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
+        }
+        static bool IsWinner(int[,] board)
+        {
+            // Перевірте, чи всі кораблі потоплені.
+            for (int x = 0; x < 10; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    if (board[x, y] > 0)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
         static void Main(string[] args)
         {
+            int[,] playerBoard = new int[10, 10];
+            int[,] computerBoard = new int[10, 10];
+            PlaceShips(playerBoard);
+            PlaceShips(computerBoard);
+
+            while (true)
+            {
+                // Виведіть поле бою.
+                Console.Clear();
+                Console.WriteLine("Your Board:");
+                DrawBoard(playerBoard);
+                Console.WriteLine("Computer's Board:");
+                DrawBoard(computerBoard);
+
+                // Обробіть хід користувача.
+                PlayerTurn(computerBoard);
+
+                // Перевірте, чи є переможець.
+                if (IsWinner(playerBoard))
+                {
+                    Console.WriteLine("Congratulations, you won!");
+                    break;
+                }
+
+                // Обробіть хід комп'ютера.
+                ComputerTurn(playerBoard);
+
+                // Перевірте, чи є переможець.
+                if (IsWinner(computerBoard))
+                {
+                    Console.WriteLine("Sorry, you lost.");
+                    break;
+                }
+            }
+
+            // Завершіть гру.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
         }
     }
 }
