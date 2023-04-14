22b702b56996e83f4f4e86c1758db21857b6d94c
StanTheMove <stas.kholopkin@gmail.com>
2023-04-14 20:15:09 +0300

    Added whole project

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattle_Copy_paste____
+{
+    internal class Program
+    {
+        static int[,] playerBoard = new int[10, 10];
+        static int[,] computerBoard = new int[10, 10];
+        static void PlaceShips(int[,] board)
+        {
+            Random random = new Random();
+            int numShips = 3;
+
+            for (int i = 0; i < numShips; i++)
+            {
+                int x = random.Next(0, 10);
+                int y = random.Next(0, 10);
+
+                // Перевірте, чи є корабель в цій позиції.
+                if (board[x, y] == 1)
+                {
+                    // Якщо так, повторіть спробу.
+                    i--;
+                }
+                else
+                {
+                    // Якщо ні, розмістіть корабель.
+                    board[x, y] = 1;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
